198378627e0eb0ef26f64db05b1c8fa7bb6caff2
QZ <32336293+sonofbiscuit@users.noreply.github.com>
2019-01-02 17:18:00 +0800

    Add files via upload

---
+++ road/AMapUtil.java
@@ -0,0 +1,23 @@
+//工具类
+
+public class AMapUtil {
+
+    /**
+     * 把LatLonPoint对象转化为LatLon对象
+     */
+    public static LatLng convertToLatLng(LatLonPoint latLonPoint) {
+        return new LatLng(latLonPoint.getLatitude(), latLonPoint.getLongitude());
+    }
+
+    /**
+     * 把集合体的LatLonPoint转化为集合体的LatLng
+     */
+    public static ArrayList<LatLng> convertArrList(List<LatLonPoint> shapes) {
+        ArrayList<LatLng> lineShapes = new ArrayList<>();
+        for (LatLonPoint point : shapes) {
+            LatLng latLngTemp = AMapUtil.convertToLatLng(point);
+            lineShapes.add(latLngTemp);
+        }
+        return lineShapes;
+    }
+}
